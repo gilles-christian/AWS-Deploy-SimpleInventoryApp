4006a87cd8d5ccb3075f2069ea501c4c6acf99d8
gilles-christian <mathias762001-2@yahoo.fr>
2022-09-19 13:22:55 +0100

    initupdate
    init script update

--- data.sh
+++ data.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-yum update -y
-yum install -y httpd.x86_64
-systemctl start httpd.service
-systemctl enable httpd.service
-echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+sudo yum update -y
+sudo yum install git -y
+sudo yum install gcc g++ make -y
+curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
+sudo dnf install nodejs
+git clone https://github.com/gilles-christian/simple-inventory-app-lab
+cd ./simple-inventory-app-lab
+sudo npm run build:production
+sudo npm run start
